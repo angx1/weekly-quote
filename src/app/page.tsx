53ab6b8779cc11c9172722a9ff921bcb9af60fee
angel <angel.rodriguez3rm@gmail.com>
2025-09-10 11:47:07 +0200

    Added is on quote tags for better scrapping

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,24 @@ export default function Home() {
   return (
     <div className="--font-geist-mono grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <blockquote className="text-2xl md:text-3xl text-gray-800 dark:text-gray-200 mb-6">
+        <blockquote
+          className="text-2xl md:text-3xl text-gray-800 dark:text-gray-200 mb-6"
+          id="cita-semanal"
+        >
           {weeklyQuoteData.quote}
         </blockquote>
         {weeklyQuoteData.author ? (
-          <cite className="text-lg text-gray-600 dark:text-gray-400">
+          <cite
+            className="text-lg text-gray-600 dark:text-gray-400"
+            id="autor-cita-semanal"
+          >
             — {weeklyQuoteData.author}
           </cite>
         ) : null}
-        <p className="text-sm text-gray-500 dark:text-gray-500 mt-4">
+        <p
+          className="text-sm text-gray-500 dark:text-gray-500 mt-4"
+          id="semana-cita-semanal"
+        >
           Semana {weeklyQuoteData.week}
         </p>
       </main>
